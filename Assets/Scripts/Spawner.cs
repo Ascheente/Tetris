64add5cc4798d6d1dc3fabe95e0b63f3e75eb93f
Ascheente <joaovitorquental@gmail.com>
2020-08-08 00:22:21 -0300

    Adição de comentario no script de Spawn

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -18,7 +18,7 @@ public class Spawner : MonoBehaviour {
 
 	public void spawnNext() {
 		int i = Random.Range (0, groups.Length);
-
+        //está linha serve para instanciar os objetos
 		Instantiate (groups [i], transform.position, Quaternion.identity);
 	}
 }
